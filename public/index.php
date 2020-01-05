b327d8a42a33965f6954c7a375e85ea4571e9ade
Leonid Mistyukov <downtempa@mail.ru>
2020-01-05 23:59:19 +0300

    add simple search form

--- public/index.php
+++ public/index.php
@@ -6,6 +6,8 @@ require __DIR__ . '/../vendor/autoload.php';
 use Slim\Factory\AppFactory;
 use DI\Container;
 
+$users = ['mike', 'mishel', 'adel', 'keks', 'kamila'];
+
 $container = new Container();
 $container->set('renderer', function () {
   return new \Slim\Views\PhpRenderer(__DIR__ . '/../templates');
@@ -25,21 +27,41 @@ $app->get('/users/{id}', function ($request, $response, $args) {
 });
 
 
-$app->get('/', function ($request, $response) {
-    return $response->write('Welcome to Slim!');
-});
+// $app->get('/', function ($request, $response) {
+//     return $response->write('Welcome to Slim!');
+// });
 
-$app->get('/users', function ($request, $response) {
-  return $response->write('GET /users');
-});
+// $app->get('/users', function ($request, $response) {
+//   return $response->write('GET /users');
+// });
 
-$app->post('/users', function ($request, $response) {
-  return $response->withStatus(302);
-});
+// $app->post('/users', function ($request, $response) {
+//   return $response->withStatus(302);
+// });
 
 $app->get('/courses/{id}', function ($request, $response, array $args) {
   $courseId = $args['id'];
   return $response->write("Course id: {$courseId}");
 });
+
+$app->get('/users', function ($request, $response) use ($users) {
+  $search = $request->getQueryParam('search');
+  $userList = isset($search) ? filterUsersByName($users, $search) : $users;
+
+  $params = [
+    'users' => $userList,
+  ];
+
+  return $this->get('renderer')->render($response, 'users/list.phtml', $params);
+});
+
+function filterUsersByName($users, $search)
+{
+  $filteredUsersByName = array_filter($users, function ($userName) use ($search) {
+    return strpos($userName, $search) !== false;
+  });
+
+  return $filteredUsersByName;
+}
 
 $app->run();
